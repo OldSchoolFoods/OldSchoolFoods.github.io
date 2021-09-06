de483305e4c5e88cd0225aa42a43028b159dce43
Dee0954 <dee.vishwanathan@gmail.com>
2021-09-06 15:24:19 +0530

    transfer

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+const {
+  DateTime
+} = require("luxon");
+const readingTime = require('eleventy-plugin-reading-time');
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+  eleventyConfig.addPlugin(readingTime);
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
+}
